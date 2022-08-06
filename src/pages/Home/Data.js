c89c0df887bc9c4838544eb02c83e382787a9bfb
Caleb Coughenour <CalebCoughenour@protonmail.com>
2022-08-06 08:13:52 -0700

    beging adding home page with data for info sections

---
+++ src/pages/Home/Data.js
@@ -0,0 +1,63 @@
+export const homeObjOne = {
+  lightBg: false,
+  primary: true,
+  imgStart: '',
+  lightTopLine: true,
+  lightTextDesc: true,
+  buttonLabel: '',
+  description: 'Here at Hiking Spotter, we believe in delivering the high quality our customers deserve by providing access to our exclusive hiking trail list and maps',
+  headline: 'Pacific North West Hiking Locations',
+  lightText: true,
+  topLine: 'Hiking Spotter',
+  img: require('../../images/Bridge.jpg'),
+  alt: 'Image',
+  start: ''
+};
+
+export const homeObjTwo = {
+  lightBg: true,
+  primary: false,
+  imgStart: 'start',
+  lightTopLine: false,
+  lightTextDesc: false,
+  buttonLabel: 'Get Started',
+  description: 'We help business owners increase their revenue. Our team of unique specialist can help you achieve your business goals.',
+  headline: 'Lead Generation Specialist for Online Businesses',
+  lightText: false,
+  topLine: 'Marketing Agency',
+  img: require('../../images/png-2.png'),
+  alt: 'Image',
+  start: 'true'
+};
+
+export const homeObjThree = {
+  lightBg: false,
+  primary: true,
+  imgStart: '',
+  lightTopLine: true,
+  lightTextDesc: true,
+  buttonLabel: 'Get Started',
+  description: 'We help business owners increase their revenue. Our team of unique specialist can help you achieve your business goals.',
+  headline: 'Lead Generation Specialist for Online Businesses',
+  lightText: true,
+  topLine: 'Marketing Agency',
+  img: require('../../images/png-3.png'),
+  alt: 'Image',
+  start: ''
+};
+
+export const homeObjFour = {
+  lightBg: false,
+  primary: true,
+  imgStart: 'true',
+  lightTopLine: true,
+  lightTextDesc: true,
+  buttonLabel: 'Get Started',
+  description: 'We help business owners increase their revenue. Our team of unique specialist can help you achieve your business goals.',
+  headline: 'Lead Generation Specialist for Online Businesses',
+  lightText: true,
+  topLine: 'Marketing Agency',
+  img: require('../../images/png-4.png'),
+  alt: 'Image',
+  start: 'true'
+};
