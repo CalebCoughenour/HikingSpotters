6ce8cfbda6d7259b3af772a8921bf5995645f4a1
Caleb Coughenour <CalebCoughenour@protonmail.com>
2022-08-07 09:47:02 -0700

    add image slider and second info section to homepage

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import { homeObjOne } from './Data';
-import { InfoSection } from '../../components';
+import { homeObjOne, homeObjTwo } from './Data';
+import { ImageSlider, InfoSection } from '../../components';
+import { SliderData } from '../Photos/Data';
 
 const Home = () => {
   return (
     <>
       <InfoSection {...homeObjOne} />
+      <ImageSlider slides={SliderData} />
+      <InfoSection {...homeObjTwo} />
     </>
   )
 }
