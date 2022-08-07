7df0e7a3d577d2f47a7ea7cee8aa9f63839202be
Caleb Coughenour <CalebCoughenour@protonmail.com>
2022-08-07 09:45:17 -0700

    add photo routes and alts for slider

---
+++ src/pages/Photos/Data.js
@@ -0,0 +1,22 @@
+export const SliderData = [
+  {
+    image: require('../../images/Waterfall.jpg'),
+    alt: 'A photo of a large waterfall with people swimming in the pool below'
+  },
+  {
+    image: require('../../images/Lacamas-Park-Bridge.jpg'),
+    alt: 'A bridge surrounded by wooded area'
+  },
+  {
+    image: require('../../images/Lacamas-Park-Sun-through-trees.jpg'),
+    alt: 'Sun shining through moss covered branches and trees'
+  },
+  {
+    image: require('../../images/Creek-Bed.jpg'),
+    alt: 'A photo of a wide creek with rushing water'
+  },
+  {
+    image: require('../../images/Path2.jpg'),
+    alt: 'A photo of a path that leads through a wooded area'
+  }
+];
